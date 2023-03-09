27608e715806b81a58ee71f16f6b1f05520735ae
Zahar <dendy.wool@mail.ru>
2023-03-09 19:37:59 +0100

    lesson 94 - почти ничего непонятно

--- js/script.js
+++ js/script.js
@@ -324,6 +324,7 @@ window.addEventListener('DOMContentLoaded', () => {
     //Слайды
 
     const prevSlider = document.querySelector('.offer__slider-prev'),
+        slider = document.querySelector('.offer__slider'),
         currentIndex = document.querySelector('#current'),
         totalIndex = document.querySelector('#total'),
         nextSlider = document.querySelector('.offer__slider-next'),
@@ -334,7 +335,7 @@ window.addEventListener('DOMContentLoaded', () => {
     let counter = 1,
         offset = 0;
 
-     totalIndex.innerHTML = `${getZero(Sliders.length)}`;
+    totalIndex.innerHTML = `${getZero(Sliders.length)}`;
 
     slidersField.style.width = 100 * Sliders.length + '%';
     slidersField.style.display = 'flex';
@@ -343,6 +344,58 @@ window.addEventListener('DOMContentLoaded', () => {
     slidersWrapper.style.overflow = 'hidden';
 
     Sliders.forEach(slider => slider.style.width = width);
+
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+        dots = [];
+    indicators.classList.add('carousel-indicators');
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+    slider.append(indicators);
+
+    for (let i = 0; i < Sliders.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.style.cssText = `
+        box-sizing: content-box;
+        flex: 0 1 auto;
+        width: 30px;
+        height: 6px;
+        margin-right: 3px;
+        margin-left: 3px;
+        cursor: pointer;
+        background-color: #fff;
+        background-clip: padding-box;
+        border-top: 10px solid transparent;
+        border-bottom: 10px solid transparent;
+        opacity: .5;
+        transition: opacity .6s ease;
+        `;
+
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    function forDots(dots, opacity) {
+        dots.forEach((dot) => {
+            dot.style.opacity = opacity
+        });
+        dots[counter - 1].style.opacity = 1;
+    }
 
     nextSlider.addEventListener('click', () => {
         if (offset == +width.slice(0, width.length - 2) * (Sliders.length - 1)) {
@@ -361,6 +414,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
         currentIndex.textContent = `${getZero(counter)}`;
 
+        forDots(dots, '.5');
     });
 
     prevSlider.addEventListener('click', () => {
@@ -382,44 +436,21 @@ window.addEventListener('DOMContentLoaded', () => {
 
     });
 
-    // currentIndex.innerHTML = ``;
-    // totalIndex.innerHTML = `${getZero(Sliders.length)}`;
+    forDots(dots, '.5');
 
-    // showSlider(0);
+    dots.forEach((dot) => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
 
-    // function showSlider(i) {
-    //     currentIndex.innerHTML = `${getZero(i + 1)}`;
-    //     Sliders.forEach((item, index) => {
-    //         if (index == i) {
-    //             item.classList.remove('hide');
-    //             item.classList.add('show', 'fade');
-    //         } else {
-    //             item.classList.remove('show');
-    //             item.classList.add('hide', 'fade')
-    //         }
-    //     })
+            counter = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
 
-    // }//просто так включил
+            slidersField.style.transform = `translateX(-${offset}px)`;
 
-    // // console.log(Sliders.length);
-    // // console.log(Sliders)
+            currentIndex.textContent = `${getZero(counter)}`;
 
-    // nextSlider.addEventListener('click', () => {
-    //     if (counter == Sliders.length - 1) {
-    //         counter = 0;
-    //         showSlider(counter);
-    //     } else {
-    //         showSlider(++counter);
-    //     }
-    // });
-
-    // prevSlider.addEventListener('click', () => {
-    //     if (counter == 0) {
-    //         counter = Sliders.length - 1;
-    //         showSlider(counter);
-    //     } else {
-    //         showSlider(--counter);
-    //     }
-    // })
+            forDots(dots, '.5');
+        })
+    })
 
 });
